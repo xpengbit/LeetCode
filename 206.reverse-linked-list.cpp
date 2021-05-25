38930b48621c122e65bd06e44f421e1c844be14c
xpengbit <xpeng61873@gmail.com>
2021-05-24 20:26:08 -0500

    LeetCode 206

---
+++ 206.reverse-linked-list.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL) return NULL;
+        ListNode* cur, *pre, *next;
+        cur = head;
+        pre = NULL;
+        while(cur != NULL){
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;    
+        }
+        return pre;
+        //return reverse(head, NULL);
+    }
+    /*use recursive*/
+    /*ListNode* reverse(ListNode* cur, ListNode* pre ){
+        if(cur == NULL) return pre;
+        ListNode* next = cur->next;
+        cur->next = pre;
+        return reverse(next, cur);
+    }*/
+};
+// @lc code=end
+
