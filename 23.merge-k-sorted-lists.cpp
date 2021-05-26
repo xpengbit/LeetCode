0608e95f0cc9212131a3dacb9e1d9ca6a0237d06
xpengbit <xpeng61873@gmail.com>
2021-05-25 22:48:11 -0500

    LeetCode 23

--- 23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists.cpp
@@ -14,8 +14,10 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
-class Solution {
+ *
+ * /
+ /* Solution use PQ*/ 
+/*class Solution {
 public:
     struct compare{
         bool operator()(ListNode* l1, ListNode* l2){
@@ -32,7 +34,7 @@ public:
         };
         priority_queue<ListNode*, vector<ListNode*>, decltype(comp)> pq(comp);
         */
-        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+/*        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
 
         for(auto it : lists){
             if(it)  pq.push(it);
@@ -48,6 +50,40 @@ public:
                 pq.push(node->next); 
         }
         return Dummy->next;
+    }
+};*/
+
+
+/*Solution use Divid and Conquer*/
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return partion(lists, 0, lists.size() - 1); 
+    }
+
+    ListNode* partion(vector<ListNode*>& lists, int start, int end){
+        if(start == end) return lists[start];
+
+        if(start < end){
+            int mid = start + (end - start)/2;
+            ListNode* l = partion(lists, start, mid);
+            ListNode* r = partion(lists, mid + 1, end);
+            return merge(l, r);
+        } 
+        return NULL;
+    }
+
+    ListNode* merge(ListNode* l, ListNode* r){
+        if(l == NULL) return r;
+        if(r == NULL) return l;
+        if(l->val < r->val){ 
+            l->next = merge(l->next, r);
+            return l;
+        }
+        else{
+            r->next = merge(l, r->next);
+            return r;
+        }
     }
 };
 // @lc code=end
