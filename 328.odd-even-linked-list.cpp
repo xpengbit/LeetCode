729780863999f8d39cae913e0c6307127f666253
xpengbit <xpeng61873@gmail.com>
2021-05-30 20:59:09 -0500

    LeetCode 328

---
+++ 328.odd-even-linked-list.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=328 lang=cpp
+ *
+ * [328] Odd Even Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL or head->next == NULL)
+            return head;
+        
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenStart = even;
+        while(even != NULL and even->next != NULL){
+            odd->next = odd->next->next;
+            odd = odd->next;
+            even->next = even->next->next;
+            even = even->next;
+        }
+        odd->next = evenStart;
+        return head;
+    }
+};
+// @lc code=end
+
